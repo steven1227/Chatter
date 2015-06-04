5755b1b051ee6374b99823910c8485cba13b6b68
steven <rendong_liu@hotmail.com>
2015-06-04 21:44:37 +0200

    add the chat list

--- app/src/main/java/com/example/steven/chat/UserList.java
+++ app/src/main/java/com/example/steven/chat/UserList.java
@@ -1,13 +1,23 @@
 package com.example.steven.chat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.ParseUser;
 import com.parse.FindCallback;
@@ -35,6 +45,37 @@ public class UserList extends Activity {
                     @Override
                     public void done(List<ParseUser> list, ParseException e) {
                         dia.dismiss();
+                        if (list != null) {
+                            Log.e("help", " " + list.size());
+                            if (list.size() == 0) {
+                                Toast.makeText(getApplicationContext(), "Your do not have any friends now", Toast.LENGTH_SHORT).show();
+                            } else {
+                                ListView chatlist=(ListView)findViewById(R.id.chatlist);
+                                userlist=new ArrayList<ParseUser>(list);
+                                chatlist.setAdapter(new UserAdapter());
+                                chatlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                    @Override
+                                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                        Intent a=new Intent(UserList.this,Chat.class);
+                                        startActivity(a);
+                                    }
+                                });
+
+                            }
+
+
+                        } else {
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(UserList.this);
+                            builder1.setMessage(e.getMessage());
+                            builder1.setPositiveButton("OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                            AlertDialog alert11 = builder1.create();
+                            alert11.show();
+                        }
                     }
                 }
         );
@@ -82,5 +123,33 @@ public class UserList extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class UserAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return userlist.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return userlist.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView==null){
+                convertView=getLayoutInflater().inflate(R.layout.chatitem,null);
+            }
+            ((TextView)convertView).setText(userlist.get(position).getUsername());
+            ((TextView)convertView).setCompoundDrawablesRelativeWithIntrinsicBounds((userlist.get(position)).getBoolean("online") ? R.drawable.ic_online
+                    : R.drawable.ic_offline, 0, R.drawable.arrow, 0);
+            return convertView;
+        }
     }
 }
